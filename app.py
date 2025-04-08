859956f90252f768c076d3ccd00e62d6de9c3472
Sarthak Patel <sarthakpatel1708@gmail.com>
2025-04-08 22:46:39 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+import os
+import gzip
+import shutil
+
+# TMDB API key
+API_KEY =os.getenv("API_KEY")
+
+# Load the pre-saved data
+movie_dict = pickle.load(open(r'D:\Python\Major_Project\Movie_recommendation_system\movie_list.pkl', 'rb'))
+with gzip.open(r'D:\Python\Major_Project\Movie_recommendation_system\similarity.pkl.gz', 'rb') as f:
+    similarity = pickle.load(f)
+
+movies = pd.DataFrame(movie_dict)
+
+# Custom CSS for movie theme
+st.markdown("""
+    <style>
+    body {
+        background-image: url('https://wallpaperaccess.com/full/329583.jpg');
+        background-size: cover;
+    }
+    .stApp {
+        background-color: rgba(0, 0, 0, 0.6);
+        color: white;
+    }
+    .css-1d391kg, .css-1v3fvcr {
+        background-color: rgba(0, 0, 0, 0.7) !important;
+        color: white !important;
+    }
+    h1 {
+        color: #FF4B4B;
+        text-shadow: 1px 1px 2px black;
+    }
+    .stButton>button {
+        background-color: #FF4B4B;
+        color: white;
+        font-weight: bold;
+    }
+    .stSelectbox > div {
+        color: black;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        print("API Response:", data)  # Debug
+        poster_path = data.get('poster_path')
+
+        if poster_path:
+            full_url = "https://image.tmdb.org/t/p/w500/" + poster_path
+            print("Poster URL:", full_url)
+            return full_url
+        else:
+            print("Poster not found.")
+            return "https://via.placeholder.com/500x750?text=No+Image"
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching poster for movie_id {movie_id}: {e}")
+        return "https://via.placeholder.com/500x750?text=Error"
+
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    
+    recommended_movies = []
+    recommended_posters = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_posters
+
+# Streamlit UI
+st.title('🎬 Movie Recommendation System')
+
+selected_movie_name = st.selectbox("Choose a movie", movies['title'].values)
+
+if st.button('Show Recommendations'):
+    names, posters = recommend(selected_movie_name)
+    
+    st.subheader("You might also like:")
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
---
+++ movie_recommendation_system.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np 
+import ast 
+import nltk 
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+
+movie=pd.read_csv(r"D:\Datasets\tmdb_5000_movies.csv\tmdb_5000_movies.csv")
+credits=pd.read_csv(r"D:\Datasets\tmdb_5000_credits.csv\tmdb_5000_credits.csv")
+# print(movie.head())
+# print(credits.head())
+
+movie=movie.merge(credits,on='title')
+# print(movie.head())
+
+movie=movie[['movie_id','title','overview','genres','keywords','cast','crew']]
+print(movie.head())
+
+print(movie.isnull().sum())
+
+movie.dropna(inplace=True)
+print(movie.isnull().sum())
+
+print(movie.duplicated().sum())
+
+print(movie.iloc[0].genres)
+
+def convert(obj):
+    L=[]
+    for i in ast.literal_eval(obj):
+        L.append(i['name'])
+    return L
+
+movie['genres']=movie['genres'].apply(convert)
+
+movie['keywords']=movie['keywords'].apply(convert)
+
+# print(movie.head())
+
+def convert3(obj):
+    L=[]
+    counter=0
+    for i in ast.literal_eval(obj):
+        if counter!=3:
+            L.append(i['name'])
+            counter+=1
+        else:
+            break
+    return L
+
+movie['cast']=movie['cast'].apply(convert3)
+# print(movie.head())
+
+def fetch_director(obj):
+    L=[]
+    for i in ast.literal_eval(obj):
+        if i['job']=='Director':
+            L.append(i['name'])
+            break
+    return L
+
+movie['crew']=movie['crew'].apply(fetch_director)
+# print(movie.head())
+
+movie['overview']=movie['overview'].apply(lambda x:x.split())
+# print(movie.head())
+
+movie['genres']=movie['genres'].apply(lambda x:[i.replace(" ","")for i in x])
+movie['keywords']=movie['keywords'].apply(lambda x:[i.replace(" ","")for i in x])
+movie['cast']=movie['cast'].apply(lambda x:[i.replace(" ","")for i in x])
+movie['crew']=movie['crew'].apply(lambda x:[i.replace(" ","")for i in x])
+# print(movie.head())
+
+movie['tags']=movie['overview']+movie['cast']+movie['crew']+movie['genres']+movie['keywords']
+# print(movie.head())
+
+new_df=movie[['movie_id','title','tags']]
+# print(new_df.head())
+
+new_df['tags']=new_df['tags'].apply(lambda x:" ".join(x))
+# print(new_df.head())
+
+new_df['tags']=new_df['tags'].apply(lambda x:x.lower())
+print(new_df.head())
+
+ps=PorterStemmer()
+
+def stem(text):
+    y=[]
+    for i in text.split():
+        y.append(ps.stem(i))
+    return " ".join(y)
+
+new_df['tags']=new_df['tags'].apply(stem)
+
+cv=CountVectorizer(max_features=5000,stop_words='english')
+
+vectors=cv.fit_transform(new_df['tags']).toarray()
+print(vectors)
+
+print(cv.get_feature_names_out())
+
+similarity=cosine_similarity(vectors)
+print(similarity)
+
+def recommend(movie):
+    movie_index=new_df[new_df['title']==movie].index[0]
+    distances=similarity[movie_index]
+    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+
+    for i in movies_list:
+        print(new_df.iloc[i[0]].title)
+    
+recommend('Cars')
+
+
+
+pickle.dump(new_df, open(r'D:\Python\Major_Project\Movie_recommendation_system\movie_list.pkl', 'wb'))
+pickle.dump(similarity, open(r'D:\Python\Major_Project\Movie_recommendation_system\similarity.pkl', 'wb'))
